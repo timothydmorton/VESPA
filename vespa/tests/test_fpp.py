a7270057b068ee4aac95c1fd719e5c10ffb07807
Timothy Morton <tim.morton@gmail.com>
2015-03-19 03:55:41 -0400

    added FPPCalculation.from_ini unit test

--- vespa/tests/test_fpp.py
+++ vespa/tests/test_fpp.py
@@ -1,3 +1,11 @@
 from __future__ import print_function, division
 
+import os, os.path
+from vespa.fpp import FPPCalculation
+
+INI_FILE = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                'fpp.ini'))
+
+def test_fpp(ini_file=INI_FILE):
+    f = FPPCalculation.from_ini('fpp.ini', n=100)
 
