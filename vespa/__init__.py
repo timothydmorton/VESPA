18f2ba93b4aa75b7212b8aaf14a24523e05b93fa
Timothy Morton <tim.morton@gmail.com>
2016-05-09 23:28:06 -0400

    another rtd fix

--- vespa/__init__.py
+++ vespa/__init__.py
@@ -7,40 +7,45 @@ except NameError:
 
 if not __VESPA_SETUP__:
 
-    __all__ = ['FPPCalculation',
-               'EclipsePopulation', 'EBPopulation',
-               'HEBPopulation', 'BEBPopulation',
-               'PlanetPopulation', 'PopulationSet',
-               'StarPopulation', 'MultipleStarPopulation',
-               'ColormatchMultipleStarPopulation',
-               'Spectroscopic_MultipleStarPopulation',
-               'BGStarPopulation', 'BGStarPopulation_TRILEGAL',
-               'BinaryPopulation', 'Simulated_BinaryPopulation',
-               'Raghavan_BinaryPopulation', 'TriplePopulation',
-               'MAInterpolationFunction']
+    on_rtd = os.environ.get('READTHEDOCS') == 'True'
 
-    #StarPopulation & children
-    from .stars.populations import StarPopulation
-    from .stars.populations import MultipleStarPopulation
-    from .stars.populations import ColormatchMultipleStarPopulation
-    from .stars.populations import Spectroscopic_MultipleStarPopulation
-    from .stars.populations import BGStarPopulation, BGStarPopulation_TRILEGAL
-    from .stars.populations import BinaryPopulation
-    from .stars.populations import Simulated_BinaryPopulation
-    from .stars.populations import Raghavan_BinaryPopulation
-    from .stars.populations import TriplePopulation
-    
-    from .transit_basic import MAInterpolationFunction
+    if not on_rtd:
+
+        __all__ = ['FPPCalculation',
+                   'EclipsePopulation', 'EBPopulation',
+                   'HEBPopulation', 'BEBPopulation',
+                   'PlanetPopulation', 'PopulationSet',
+                   'StarPopulation', 'MultipleStarPopulation',
+                   'ColormatchMultipleStarPopulation',
+                   'Spectroscopic_MultipleStarPopulation',
+                   'BGStarPopulation', 'BGStarPopulation_TRILEGAL',
+                   'BinaryPopulation', 'Simulated_BinaryPopulation',
+                   'Raghavan_BinaryPopulation', 'TriplePopulation',
+                   'MAInterpolationFunction']
 
 
-    #EclipsePopulation & children
-    from .populations import EclipsePopulation
-    from .populations import EBPopulation, HEBPopulation, BEBPopulation
-    from .populations import PlanetPopulation
-    from .populations import PopulationSet
+        #StarPopulation & children
+        from .stars.populations import StarPopulation
+        from .stars.populations import MultipleStarPopulation
+        from .stars.populations import ColormatchMultipleStarPopulation
+        from .stars.populations import Spectroscopic_MultipleStarPopulation
+        from .stars.populations import BGStarPopulation, BGStarPopulation_TRILEGAL
+        from .stars.populations import BinaryPopulation
+        from .stars.populations import Simulated_BinaryPopulation
+        from .stars.populations import Raghavan_BinaryPopulation
+        from .stars.populations import TriplePopulation
 
-    #from .populations import calculate_eclipses
-    from .transitsignal import TransitSignal
+        from .transit_basic import MAInterpolationFunction
 
-    from .fpp import FPPCalculation
+
+        #EclipsePopulation & children
+        from .populations import EclipsePopulation
+        from .populations import EBPopulation, HEBPopulation, BEBPopulation
+        from .populations import PlanetPopulation
+        from .populations import PopulationSet
+
+        #from .populations import calculate_eclipses
+        from .transitsignal import TransitSignal
+
+        from .fpp import FPPCalculation
 
