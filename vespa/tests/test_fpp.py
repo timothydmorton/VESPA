fdb3356b1202787fe6fa94aa8836049d8c262024
Timothy Morton <tim.morton@gmail.com>
2016-07-28 14:39:00 -0700

    tests updated for contrast curves

--- vespa/tests/test_fpp.py
+++ vespa/tests/test_fpp.py
@@ -8,7 +8,14 @@ import pkg_resources
 FOLDER = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 
 def test_fpp(folder=FOLDER):
-    f = FPPCalculation.from_ini(folder, n=100)
+    f = FPPCalculation.from_ini(folder, n=100, recalc=True)
     f.FPP()
 
+def test_fpp_cc(folder=FOLDER):
+    f = FPPCalculation.from_ini(folder, ini_file='fpp_cc.ini', n=100)
+    f.FPP()
+
+def test_fpp_cc2(folder=FOLDER):
+    f = FPPCalculation.from_ini(folder, ini_file='fpp_cc2.ini', n=100)
+    f.FPP()
 
