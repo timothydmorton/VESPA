ee030cdf9de8f288de3afb76f0cb68121fe48a70
Timothy Morton <tim.morton@gmail.com>
2016-11-15 11:00:53 -0500

    requiring isochrones=0.9.1

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(name = "VESPA",
         'Topic :: Scientific/Engineering :: Astronomy'
         ],
       install_requires=['pandas>=0.13','simpledist>=0.1.11', 'emcee', 
-                        'isochrones>=0.8.1', 'acor', 'numba>=0.20', 'batman-package>=2.1', 
+                        'isochrones=0.9.1', 'acor', 'numba>=0.20', 'batman-package>=2.1', 
                         'configobj'],
       zip_safe=False
 ) 
