bc712ca15b6b9950dc6cb4f4b67f49afd2087f8f
Timothy Morton <tdm@astro.princeton.edu>
2018-05-11 11:54:50 -0700

    update unit tests

--- vespa/tests/test_fpp.py
+++ vespa/tests/test_fpp.py
@@ -5,17 +5,19 @@ from vespa.fpp import FPPCalculation
 
 import pkg_resources
 
-FOLDER = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 
-def test_fpp(folder=FOLDER):
-    f = FPPCalculation.from_ini(folder, n=100, recalc=True)
+def test_fpp(dirname='kepler-22'):
+    dirname = os.path.join(ROOT,dirname)
+    f = FPPCalculation.from_ini(dirname, n=100, recalc=True)
     f.FPP()
 
-def test_fpp_cc(folder=FOLDER):
-    f = FPPCalculation.from_ini(folder, ini_file='fpp_cc.ini', n=100)
+def test_fpp_cc(dirname='kepler-22'):
+    dirname = os.path.join(ROOT, dirname)
+    f = FPPCalculation.from_ini(dirname, ini_file='fpp_cc.ini', n=100)
     f.FPP()
 
-def test_fpp_cc2(folder=FOLDER):
-    f = FPPCalculation.from_ini(folder, ini_file='fpp_cc2.ini', n=100)
+def test_fpp_cc2(dirname='kepler-22'):
+    dirname = os.path.join(ROOT, dirname)
+    f = FPPCalculation.from_ini(dirname, ini_file='fpp_cc2.ini', n=100)
     f.FPP()
-
--- vespa/tests/test_populations.py
+++ vespa/tests/test_populations.py
@@ -1,5 +1,11 @@
 from __future__ import print_function, division
 
+from pkg_resources import resource_filename
+
+import os, os.path
+import tempfile
+import tables as tb
+
 from vespa.populations import HEBPopulation
 from vespa.populations import EBPopulation
 from vespa.populations import BEBPopulation
@@ -9,13 +15,9 @@ from isochrones.starmodel import StarModel
 from isochrones.starmodel import BinaryStarModel
 from isochrones.starmodel import TripleStarModel
 
-from pkg_resources import resource_filename
-
-import os, os.path
-import tempfile
-TMP = tempfile.gettempdir()
-
 from vespa.transit_basic import MAInterpolationFunction
+
+TMP = tempfile.gettempdir()
 
 MAfn = MAInterpolationFunction(nzs=100,nps=200,pmin=0.007,pmax=1/0.007)
 
@@ -24,12 +26,15 @@ def test_heb(filename=os.path.join(TMP,'test_heb.h5')):
             'J': 10.523,
             'K': 10.152000000000001}
     period = 289.8622
-    starmodel_file = resource_filename('vespa','tests/test_starmodel_triple.h5')
+    starmodel_file = resource_filename('vespa','tests/kepler-22/mist_starmodel_triple.h5')
     starmodel = TripleStarModel.load_hdf(starmodel_file)
     pop = HEBPopulation(mags=mags, starmodel=starmodel,
                         period=period, n=100, MAfn=MAfn)
     pop.save_hdf(filename, overwrite=True)
+    assert len(tb.file._open_files.get_handlers_by_name(filename)) == 0
+
     pop2 = HEBPopulation.load_hdf(filename)
+    assert len(tb.file._open_files.get_handlers_by_name(filename)) == 0
     assert type(pop2)==HEBPopulation
     os.remove(filename)
 
@@ -39,18 +44,21 @@ def test_eb(filename=os.path.join(TMP,'test_eb.h5')):
             'J': 10.523,
             'K': 10.152000000000001}
     period = 289.8622
-    starmodel_file = resource_filename('vespa','tests/test_starmodel_binary.h5')
+    starmodel_file = resource_filename('vespa','tests/kepler-22/mist_starmodel_binary.h5')
     starmodel = BinaryStarModel.load_hdf(starmodel_file)
     pop = EBPopulation(mags=mags, starmodel=starmodel,
                        period=period, n=100, MAfn=MAfn)
 
     pop.save_hdf(filename, overwrite=True)
+    assert len(tb.file._open_files.get_handlers_by_name(filename)) == 0
+
     pop2 = EBPopulation.load_hdf(filename)
+    assert len(tb.file._open_files.get_handlers_by_name(filename)) == 0
     assert type(pop2)==EBPopulation
     os.remove(filename)
 
 def test_beb(filename=os.path.join(TMP,'test_beb.h5')):
-    trilegal_filename = resource_filename('vespa','data/kep22field.h5')
+    trilegal_filename = resource_filename('vespa','tests/kepler-22/starfield.h5')
     mags = {'H': 10.211,
             'J': 10.523,
             'K': 10.152000000000001,
@@ -61,7 +69,11 @@ def test_beb(filename=os.path.join(TMP,'test_beb.h5')):
                         n=100, MAfn=MAfn)
 
     pop.save_hdf(filename, overwrite=True)
+    assert len(tb.file._open_files.get_handlers_by_name(filename)) == 0
+
     pop2 = BEBPopulation.load_hdf(filename)
+    assert len(tb.file._open_files.get_handlers_by_name(filename)) == 0
+
     assert type(pop2)==BEBPopulation
     os.remove(filename)
 
@@ -70,12 +82,15 @@ def test_pl(filename=os.path.join(TMP,'test_pl.h5')):
     radius = (0.91,0.03)
     period = 289.8622
     rprs = 0.02
-    starmodel_file = resource_filename('vespa','tests/test_starmodel_single.h5')
+    starmodel_file = resource_filename('vespa','tests/kepler-22/mist_starmodel_single.h5')
     starmodel = StarModel.load_hdf(starmodel_file)
     pop = PlanetPopulation(period=period, rprs=rprs,
                        starmodel=starmodel, n=100, MAfn=MAfn)
 
     pop.save_hdf(filename, overwrite=True)
+    assert len(tb.file._open_files.get_handlers_by_name(filename)) == 0
+
     pop2 = PlanetPopulation.load_hdf(filename)
+    assert len(tb.file._open_files.get_handlers_by_name(filename)) == 0
     assert type(pop2)==PlanetPopulation
     os.remove(filename)
