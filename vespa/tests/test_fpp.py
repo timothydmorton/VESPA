0e2ddb858fbc5189e6bc5bbb019e23726f497ad1
Timothy Morton <tdm@astro.princeton.edu>
2018-05-21 10:56:35 -0700

    update test_fpp to use unittest.TestCase

--- vespa/tests/test_fpp.py
+++ vespa/tests/test_fpp.py
@@ -1,24 +1,39 @@
 from __future__ import print_function, division
 
 import os, os.path
+import unittest
+import tempfile
+
 from vespa.fpp import FPPCalculation
 
 import pkg_resources
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 
-def test_fpp(dirname='kepler-22'):
-    dirname = os.path.join(ROOT,dirname)
-    f = FPPCalculation.from_ini(dirname, n=100, recalc=True)
-    f.FPP()
-    f.bootstrap_FPP(N=3)
+class TestFPP(unittest.TestCase):
+    name = 'kepler-22'
+    ini_file = 'fpp.ini'
+    n = 100
+    recalc = True
 
-def test_fpp_cc(dirname='kepler-22'):
-    dirname = os.path.join(ROOT, dirname)
-    f = FPPCalculation.from_ini(dirname, ini_file='fpp_cc.ini', n=100)
-    f.FPP()
+    def setUp(self):
+        dirname = os.path.join(ROOT, self.name)
+        self.f = FPPCalculation.from_ini(dirname, ini_file=self.ini_file,
+                                    n=self.n, recalc=self.recalc)
 
-def test_fpp_cc2(dirname='kepler-22'):
-    dirname = os.path.join(ROOT, dirname)
-    f = FPPCalculation.from_ini(dirname, ini_file='fpp_cc2.ini', n=100)
-    f.FPP()
+    def test_fpp(self):
+        fpp = self.f.FPP()
+        assert fpp > 0
+
+    def test_bootstrap(self):
+        h, lines = self.f.bootstrap_FPP(N=3)
+        for line in lines:
+            assert float(line.split()[-1]) > 0
+
+class TestFPP_CC(TestFPP):
+    ini_file = 'fpp_cc.ini'
+    recalc= False
+
+class TestFPP_CC2(TestFPP):
+    ini_file = 'fpp_cc2.ini'
+    recalc = False
